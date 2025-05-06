ee276a974d8b9678bf6a7b1600be68806210824c
siosio <siosio12@gmail.com>
2025-05-06 12:59:12 +0900

    fix build error

--- src/main/kotlin/siosio/kodkod/KDocGenerator.kt
+++ src/main/kotlin/siosio/kodkod/KDocGenerator.kt
@@ -1,14 +1,9 @@
 package siosio.kodkod
 
-import com.intellij.psi.PsiNameIdentifierOwner
-import org.jetbrains.kotlin.idea.debugger.sequence.psi.resolveType
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.getDeclarationBody
-import org.jetbrains.kotlin.nj2k.postProcessing.type
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtNamedDeclarationUtil
-import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.KtPsiUtil
-import org.jetbrains.kotlin.types.typeUtil.isUnit
+import com.intellij.psi.*
+import org.jetbrains.kotlin.nj2k.postProcessing.*
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.types.typeUtil.*
 
 interface KDocGenerator {
 
@@ -38,7 +33,8 @@ class NamedFunctionKDocGenerator(private val function: KtNamedFunction) : KDocGe
             builder.appendLine(toParamsKdoc(params = function.valueParameters))
         }
 
-        if (function.type()?.isUnit() == false) {
+        val returnType = function.typeReference
+        if (returnType != null && !returnType.text.endsWith("Unit")) {
             builder.appendLine("* @return")
         }
         builder.appendLine("*/")
@@ -60,18 +56,18 @@ class ClassKDocGenerator(private val klass: KtClass) : KDocGenerator {
         val (properties, parameters) = klass.primaryConstructor?.valueParameters?.partition {
             it.hasValOrVar()
         } ?: Pair(emptyList(), emptyList())
-        
+
         if (properties.isNotEmpty()) {
             builder.appendLine(toParamsKdoc(keyword = "@property", params = properties))
         }
-        
+
         if (parameters.isNotEmpty()) {
             builder.appendLine("* @constructor")
                     .appendLine("* TODO")
                     .appendLine("*")
                     .appendLine(toParamsKdoc(params = parameters))
         }
-        
+
         builder.appendLine("*/")
         return builder.toString()
     }
